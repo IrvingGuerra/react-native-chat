d30fc9f374b4d47ec048fe3c04b2d8d87cb78690
Irving Guerra <guerravargasirving@gmail.com>
2020-06-03 19:44:29 -0500

    add external server

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const SERVER = 'localhost'; //35.222.104.227
-export const PORT = '3000'; //80
+export const SERVER = '34.69.35.173';
+export const PORT = '80';
 export const API_HEADERS = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
